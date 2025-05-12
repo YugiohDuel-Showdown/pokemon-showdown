6a5ad654c7db26ad3a335ceba9229d42745d4cd3
Cody Spratford <koreanpanda345@gmail.com>
2025-05-11 20:54:40 -0700

    renaming

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -1,608 +1,609 @@
-export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable = {
-	illusionm: {
-		tier: "LC",
-	},
-	darkmagician: {
-		tier: "NFE",
-	},
-	darksage: {
-		tier: "OU",
-	},
-	bchick: {
-		tier: "LC",
-	},
-	redeyes: {
-		tier: "NFE",
-	},
-	redeyesmetal: {
-		tier: "OU",
-	},
-	paladinow: {
-		tier: "LC",
-	},
-	blueeyes: {
-		tier: "NFE",
-	},
-	beudragon: {
-		tier: "OU",
-	},
-	maneater: {
-		tier: "LC",
-	},
-	neaterb: {
-		tier: "OU",
-	},
-	petitmoth: {
-		tier: "LC",
-	},
-	larvaem: {
-		tier: "NFE",
-	},
-	cocoonoe: {
-		tier: "NFE",
-	},
-	greatmoth: {
-		tier: "OU",
-	},
-	pugmoth: {
-		tier: "OU",
-	},
-	parasitec: {
-		tier: "LC",
-	},
-	psnbtrfly: {
-		tier: "OU",
-	},
-	beaverw: {
-		tier: "OU",
-	},
-	kuriboh: {
-		tier: "LC",
-	},
-	wingedkrb: {
-		tier: "NFE",
-	},
-	wkurlv10: {
-		tier: "OU",
-	},
-	kuribabyln: {
-		tier: "OU",
-	},
-	linkuriboh: {
-		tier: "OU",
-	},
-	lwinguard: {
-		tier: "LC",
-	},
-	rkwarrior: {
-		tier: "OU",
-	},
-	landstar: {
-		tier: "LC",
-	},
-	gilfordtl: {
-		tier: "OU",
-	},
-	happylover: {
-		tier: "LC",
-	},
-	shfriend: {
-		tier: "OU",
-	},
-	flamem: {
-		tier: "LC",
-	},
-	flameswrd: {
-		tier: "NFE",
-	},
-	dflarek: {
-		tier: "OU",
-	},
-	feralimp: {
-		tier: "LC",
-	},
-	desfimp: {
-		tier: "OU",
-	},
-	babyd: {
-		tier: "LC",
-	},
-	thousandd: {
-		tier: "OU",
-	},
-	jerrybean: {
-		tier: "OU",
-	},
-	witchotbf: {
-		tier: "OU",
-	},
-	sangan: {
-		tier: "LC",
-	},
-	sanwitch: {
-		tier: "OU",
-	},
-	injflily: {
-		tier: "OU",
-	},
-	betatmw: {
-		tier: 'LC',
-	},
-	valkyrion: {
-		tier: "OU",
-	},
-	alphatmw: {
-		tier: "LC",
-	},
-	berserkion: {
-		tier: 'OU',
-	},
-	gokibore: {
-		tier: "LC",
-	},
-	gokipole: {
-		tier: "OU",
-	},
-	berfomet: {
-		tier: "LC",
-	},
-	chimera: {
-		tier: "NFE",
-	},
-	chimaera: {
-		tier: "OU",
-	},
-	swampbtl: {
-		tier: "OU",
-	},
-	lavabtl: {
-		tier: 'OU',
-	},
-	killerneedle: {
-		tier: "LC",
-	},
-	armoredbee: {
-		tier: "OU",
-	},
-	pokidraco: {
-		tier: "LC",
-	},
-	doradora: {
-		tier: "OU",
-	},
-	frogthejam: {
-		tier: "LC",
-	},
-	toadallya: {
-		tier: "OU",
-	},
-	treebornfrog: {
-		tier: "OU",
-	},
-	tradetoad: {
-		tier: "LC",
-	},
-	substitoad: {
-		tier: "OU",
-	},
-	desfrog: {
-		tier: "LC",
-	},
-	d3sfrog: {
-		tier: "OU",
-	},
-	tadpole: {
-		tier: "LC",
-	},
-	beelzefrog: {
-		tier: "OU",
-	},
-	swapfrog: {
-		tier: "OU",
-	},
-	ronintoadin: {
-		tier: "OU",
-	},
-	dupefrog: {
-		tier: "OU",
-	},
-	lusterdragon: {
-		tier: "LC",
-	},
-	lusterdragon2: {
-		tier: "NFE",
-	},
-	diamondhd: {
-		tier: 'OU',
-	},
-	littled: {
-		tier: 'LC',
-	},
-	uraby: {
-		tier: 'NFE',
-	},
-	blacktyranno: {
-		tier: 'OU',
-	},
-	basicinsect: {
-		tier: 'LC',
-	},
-	armbinsect: {
-		tier: 'OU',
-	},
-	insectprincess: {
-		tier: 'LC',
-	},
-	insectqueen: {
-		tier: 'OU',
-	},
-	'2hkingrex': {
-		tier: 'OU',
-	},
-	celticguardian: {
-		tier: 'LC',
-	},
-	celticnoble: {
-		tier: 'OU',
-	},
-	ryukishin: {
-		tier: 'LC',
-	},
-	ryukpowered: {
-		tier: 'OU',
-	},
-	watapon: {
-		tier: 'LC',
-	},
-	marshmallon: {
-		tier: 'OU',
-	},
-	ojamayellow: {
-		tier: 'LC',
-	},
-	ojamaknight: {
-		tier: 'NFE',
-	},
-	ojamaking: {
-		tier: 'OU',
-	},
-	mysticalelf: {
-		tier: 'OU',
-	},
-	flamecerberus: {
-		tier: 'OU',
-	},
-	alligatorsword: {
-		tier: 'LC',
-	},
-	cyberalligator: {
-		tier: 'OU',
-	},
-	alligatorsdgn: {
-		tier: 'OU',
-	},
-	axeraider: {
-		tier: 'LC',
-	},
-	vorseraider: {
-		tier: 'OU',
-	},
-	wildheart: {
-		tier: 'LC',
-	},
-	wildwingman: {
-		tier: 'OU',
-	},
-	wingeddragon: {
-		tier: 'OU',
-	},
-	darkmagiciang: {
-		tier: 'LC',
-	},
-	dmgirltdk: {
-		tier: 'OU',
-	},
-	kairyushin: {
-		tier: 'LC',
-	},
-	odlkairyushin: {
-		tier: 'Uber',
-	},
-	harpielady: {
-		tier: "LC",
-	},
-	harpiesisters: {
-		tier: 'OU',
-	},
-	cyberharpie: {
-		tier: 'NFE',
-	},
-	cyslashharpie: {
-		tier: "OU",
-	},
-	harpiequeen: {
-		tier: "OU",
-	},
-	harpiebro: {
-		tier: "OU",
-	},
-	harpiedragon: {
-		tier: "OU",
-	},
-	ababytiger: {
-		tier: "LC",
-	},
-	amazonesstiger: {
-		tier: "OU",
-	},
-	amazonprincess: {
-		tier: "LC",
-	},
-	amazonessqueen: {
-		tier: "OU",
-	},
-	summonedskull: {
-		tier: "LC",
-	},
-	bskulldragon: {
-		tier: "OU",
-	},
-	zombieskull: {
-		tier: "OU",
-	},
-	curseofdragon: {
-		tier: 'LC',
-	},
-	gaiatdc: {
-		tier: "OU",
-	},
-	queensknight: {
-		tier: "OU",
-	},
-	kingsknight: {
-		tier: "OU",
-	},
-	jacksknight: {
-		tier: "OU",
-	},
-	archerygirl: {
-		tier: "LC",
-	},
-	saggiclown: {
-		tier: "LC",
-	},
-	petenclown: {
-		tier: "OU",
-	},
-	agido: {
-		tier: "LC",
-	},
-	agidosentinel: {
-		tier: "OU",
-	},
-	keldo: {
-		tier: "LC",
-	},
-	keldoprotector: {
-		tier: "OU",
-	},
-	blackptera: {
-		tier: "LC",
-	},
-	overtexqoatlus: {
-		tier: "OU",
-	},
-	lordofd: {
-		tier: "LC",
-	},
-	kingofd: {
-		tier: "OU",
-	},
-	gearfried: {
-		tier: "LC",
-	},
-	swordmaster: {
-		tier: "OU",
-	},
-	stonesoldier: {
-		tier: "LC",
-	},
-	steelsoldier: {
-		tier: "OU",
-	},
-	legendaryfish: {
-		tier: "LC",
-	},
-	legendaryfish2: {
-		tier: "OU",
-	},
-	koumoridragon: {
-		tier: "LC",
-	},
-	serpentdragon: {
-		tier: "OU",
-	},
-	junksynchron: {
-		tier: "LC",
-	},
-	junkwarrior: {
-		tier: "OU",
-	},
-	marron: {
-		tier: "LC",
-	},
-	skullmarron: {
-		tier: "OU",
-	},
-	maddogod: {
-		tier: "NFE",
-	},
-	behemothking: {
-		tier: "OU",
-	},
-	inpachi: {
-		tier: "LC",
-	},
-	blazinginpachi: {
-		tier: "OU",
-	},
-	charcoalinpachi: {
-		tier: "OU",
-	},
-	gaiatfknight: {
-		tier: "LC",
-	},
-	gaiaearth: {
-		tier: "OU",
-	},
-	darkcavalry: {
-		tier: "OU",
-	},
-	avian: {
-		tier: "LC",
-	},
-	flamewingman: {
-		tier: "NFE",
-	},
-	shiningwingman: {
-		tier: "OU",
-	},
-	cyberangel: {
-		tier: "LC",
-	},
-	cyberbenten: {
-		tier: "NFE",
-	},
-	cyberdakini: {
-		tier: "OU",
-	},
-	warriorzera: {
-		tier: "LC",
-	},
-	zerathemant: {
-		tier: "OU",
-	},
-	megazowler: {
-		tier: "OU",
-	},
-	illusionist: {
-		tier: "LC",
-	},
-	illusionmgc: {
-		tier: "OU",
-	},
-	parrotdragon: {
-		tier: "OU",
-	},
-	trihorndragon: {
-		tier: "OU",
-	},
-	mystictomato: {
-		tier: "LC",
-	},
-	hungryburger: {
-		tier: "OU",
-	},
-	thunderdragon: {
-		tier: "LC",
-	},
-	twinthunderd: {
-		tier: "NFE",
-	},
-	tdtitan: {
-		tier: "OU",
-	},
-	tdcolossus: {
-		tier: "OU",
-	},
-	pantherwarrior: {
-		tier: "OU",
-	},
-	musicking: {
-		tier: "LC",
-	},
-	heavymetalking: {
-		tier: "OU",
-	},
-	slotmachine: {
-		tier: "OU",
-	},
-	battleox: {
-		tier: "LC",
-	},
-	rabidhorseman: {
-		tier: "OU",
-	},
-	jiraigumo: {
-		tier: "LC",
-	},
-	launcherspider: {
-		tier: "OU",
-	},
-	vampirebat: {
-		tier: "LC",
-	},
-	vampirelord: {
-		tier: "NFE",
-	},
-	vampgenesis: {
-		tier: "OU",
-	},
-	vampirelady: {
-		tier: "NFE",
-	},
-	vampgrace: {
-		tier: "OU",
-	},
-	clayman: {
-		tier: "LC",
-	},
-	mudballman: {
-		tier: "OU",
-	},
-	skulldice: {
-		tier: "OU",
-	},
-	orgothtr: {
-		tier: "OU",
-	},
-	lajinn: {
-		tier: "OU",
-	},
-	lavagolem: {
-		tier: "OU",
-	},
-	revivaljam: {
-		tier: "LC",
-	},
-	humanoidslime: {
-		tier: "NFE",
-	},
-	humanoiddrake: {
-		tier: "OU",
-	},
-	godslime: {
-		tier: "OU",
-	},
-	pendulumm: {
-		tier: "OU",
-	},
-	speardragon: {
-		tier: "OU",
-	},
-	silentmg4: {
-		tier: "LC",
-	},
-	silentmg8: {
-		tier: "OU",
-	},
-	silentsw3: {
-		tier: "LC",
-	},
-	silentsw7: {
-		tier: "OU",
-	},
-	exodia: {
-		tier: "Uber",
-	},
-};
+export const FormatsData: import("../sim/dex-species").SpeciesFormatsDataTable =
+	{
+		illusionm: {
+			tier: "LC",
+		},
+		darkmagician: {
+			tier: "NFE",
+		},
+		darksage: {
+			tier: "OU",
+		},
+		bchick: {
+			tier: "LC",
+		},
+		redeyes: {
+			tier: "NFE",
+		},
+		redeyesmetal: {
+			tier: "OU",
+		},
+		paladinow: {
+			tier: "LC",
+		},
+		blueeyes: {
+			tier: "NFE",
+		},
+		beudragon: {
+			tier: "OU",
+		},
+		maneater: {
+			tier: "LC",
+		},
+		neaterbug: {
+			tier: "OU",
+		},
+		petitmoth: {
+			tier: "LC",
+		},
+		larvaem: {
+			tier: "NFE",
+		},
+		cocoonoe: {
+			tier: "NFE",
+		},
+		greatmoth: {
+			tier: "OU",
+		},
+		pugmoth: {
+			tier: "OU",
+		},
+		parasitec: {
+			tier: "LC",
+		},
+		psnbtrfly: {
+			tier: "OU",
+		},
+		beaverw: {
+			tier: "OU",
+		},
+		kuriboh: {
+			tier: "LC",
+		},
+		wingedkrb: {
+			tier: "NFE",
+		},
+		wkurlv10: {
+			tier: "OU",
+		},
+		kuribabyln: {
+			tier: "OU",
+		},
+		linkuriboh: {
+			tier: "OU",
+		},
+		lwinguard: {
+			tier: "LC",
+		},
+		rkwarrior: {
+			tier: "OU",
+		},
+		landstar: {
+			tier: "LC",
+		},
+		gilfordtl: {
+			tier: "OU",
+		},
+		happylover: {
+			tier: "LC",
+		},
+		shfriend: {
+			tier: "OU",
+		},
+		flamem: {
+			tier: "LC",
+		},
+		flameswrd: {
+			tier: "NFE",
+		},
+		dflarek: {
+			tier: "OU",
+		},
+		feralimp: {
+			tier: "LC",
+		},
+		desfimp: {
+			tier: "OU",
+		},
+		babyd: {
+			tier: "LC",
+		},
+		thousandd: {
+			tier: "OU",
+		},
+		jerrybean: {
+			tier: "OU",
+		},
+		witchotbf: {
+			tier: "OU",
+		},
+		sangan: {
+			tier: "LC",
+		},
+		sanwitch: {
+			tier: "OU",
+		},
+		injflily: {
+			tier: "OU",
+		},
+		betatmw: {
+			tier: "LC",
+		},
+		valkyrion: {
+			tier: "OU",
+		},
+		alphatmw: {
+			tier: "LC",
+		},
+		berserkion: {
+			tier: "OU",
+		},
+		gokibore: {
+			tier: "LC",
+		},
+		gokipole: {
+			tier: "OU",
+		},
+		berfomet: {
+			tier: "LC",
+		},
+		chimera: {
+			tier: "NFE",
+		},
+		chimaera: {
+			tier: "OU",
+		},
+		swampbtl: {
+			tier: "OU",
+		},
+		lavabtl: {
+			tier: "OU",
+		},
+		killerneedle: {
+			tier: "LC",
+		},
+		armoredbee: {
+			tier: "OU",
+		},
+		pokidraco: {
+			tier: "LC",
+		},
+		doradora: {
+			tier: "OU",
+		},
+		frogthejam: {
+			tier: "LC",
+		},
+		toadallya: {
+			tier: "OU",
+		},
+		treebornfrog: {
+			tier: "OU",
+		},
+		tradetoad: {
+			tier: "LC",
+		},
+		substitoad: {
+			tier: "OU",
+		},
+		desfrog: {
+			tier: "LC",
+		},
+		d3sfrog: {
+			tier: "OU",
+		},
+		tadpole: {
+			tier: "LC",
+		},
+		beelzefrog: {
+			tier: "OU",
+		},
+		swapfrog: {
+			tier: "OU",
+		},
+		ronintoadin: {
+			tier: "OU",
+		},
+		dupefrog: {
+			tier: "OU",
+		},
+		lusterdragon: {
+			tier: "LC",
+		},
+		lusterdragon2: {
+			tier: "NFE",
+		},
+		diamondhd: {
+			tier: "OU",
+		},
+		littled: {
+			tier: "LC",
+		},
+		uraby: {
+			tier: "NFE",
+		},
+		blacktyranno: {
+			tier: "OU",
+		},
+		basicinsect: {
+			tier: "LC",
+		},
+		armbinsect: {
+			tier: "OU",
+		},
+		insectprincess: {
+			tier: "LC",
+		},
+		insectqueen: {
+			tier: "OU",
+		},
+		"2hkingrex": {
+			tier: "OU",
+		},
+		celticguardian: {
+			tier: "LC",
+		},
+		celticnoble: {
+			tier: "OU",
+		},
+		ryukishin: {
+			tier: "LC",
+		},
+		ryukpowered: {
+			tier: "OU",
+		},
+		watapon: {
+			tier: "LC",
+		},
+		marshmallon: {
+			tier: "OU",
+		},
+		ojamayellow: {
+			tier: "LC",
+		},
+		ojamaknight: {
+			tier: "NFE",
+		},
+		ojamaking: {
+			tier: "OU",
+		},
+		mysticalelf: {
+			tier: "OU",
+		},
+		flamecerberus: {
+			tier: "OU",
+		},
+		alligatorsword: {
+			tier: "LC",
+		},
+		cyberalligator: {
+			tier: "OU",
+		},
+		alligatorsdgn: {
+			tier: "OU",
+		},
+		axeraider: {
+			tier: "LC",
+		},
+		vorseraider: {
+			tier: "OU",
+		},
+		wildheart: {
+			tier: "LC",
+		},
+		wildwingman: {
+			tier: "OU",
+		},
+		wingeddragon: {
+			tier: "OU",
+		},
+		darkmagiciang: {
+			tier: "LC",
+		},
+		dmgirltdk: {
+			tier: "OU",
+		},
+		kairyushin: {
+			tier: "LC",
+		},
+		odlkairyushin: {
+			tier: "Uber",
+		},
+		harpielady: {
+			tier: "LC",
+		},
+		harpiesisters: {
+			tier: "OU",
+		},
+		cyberharpie: {
+			tier: "NFE",
+		},
+		cyslashharpie: {
+			tier: "OU",
+		},
+		harpiequeen: {
+			tier: "OU",
+		},
+		harpiebro: {
+			tier: "OU",
+		},
+		harpiedragon: {
+			tier: "OU",
+		},
+		ababytiger: {
+			tier: "LC",
+		},
+		amazonesstiger: {
+			tier: "OU",
+		},
+		amazonprincess: {
+			tier: "LC",
+		},
+		amazonessqueen: {
+			tier: "OU",
+		},
+		summonedskull: {
+			tier: "LC",
+		},
+		bskulldragon: {
+			tier: "OU",
+		},
+		zombieskull: {
+			tier: "OU",
+		},
+		curseofdragon: {
+			tier: "LC",
+		},
+		gaiatdc: {
+			tier: "OU",
+		},
+		queensknight: {
+			tier: "OU",
+		},
+		kingsknight: {
+			tier: "OU",
+		},
+		jacksknight: {
+			tier: "OU",
+		},
+		archerygirl: {
+			tier: "LC",
+		},
+		saggiclown: {
+			tier: "LC",
+		},
+		petenclown: {
+			tier: "OU",
+		},
+		agido: {
+			tier: "LC",
+		},
+		agidosentinel: {
+			tier: "OU",
+		},
+		keldo: {
+			tier: "LC",
+		},
+		keldoprotector: {
+			tier: "OU",
+		},
+		blackptera: {
+			tier: "LC",
+		},
+		overtexqoatlus: {
+			tier: "OU",
+		},
+		lordofd: {
+			tier: "LC",
+		},
+		kingofd: {
+			tier: "OU",
+		},
+		gearfried: {
+			tier: "LC",
+		},
+		swordmaster: {
+			tier: "OU",
+		},
+		stonesoldier: {
+			tier: "LC",
+		},
+		steelsoldier: {
+			tier: "OU",
+		},
+		legendaryfish: {
+			tier: "LC",
+		},
+		legendaryfish2: {
+			tier: "OU",
+		},
+		koumoridragon: {
+			tier: "LC",
+		},
+		serpentdragon: {
+			tier: "OU",
+		},
+		junksynchron: {
+			tier: "LC",
+		},
+		junkwarrior: {
+			tier: "OU",
+		},
+		marron: {
+			tier: "LC",
+		},
+		skullmarron: {
+			tier: "OU",
+		},
+		maddogod: {
+			tier: "NFE",
+		},
+		behemothking: {
+			tier: "OU",
+		},
+		inpachi: {
+			tier: "LC",
+		},
+		blazinginpachi: {
+			tier: "OU",
+		},
+		charcoalinpachi: {
+			tier: "OU",
+		},
+		gaiatfknight: {
+			tier: "LC",
+		},
+		gaiaearth: {
+			tier: "OU",
+		},
+		darkcavalry: {
+			tier: "OU",
+		},
+		avian: {
+			tier: "LC",
+		},
+		flamewingman: {
+			tier: "NFE",
+		},
+		shiningwingman: {
+			tier: "OU",
+		},
+		cyberangel: {
+			tier: "LC",
+		},
+		cyberbenten: {
+			tier: "NFE",
+		},
+		cyberdakini: {
+			tier: "OU",
+		},
+		warriorzera: {
+			tier: "LC",
+		},
+		zerathemant: {
+			tier: "OU",
+		},
+		megazowler: {
+			tier: "OU",
+		},
+		illusionist: {
+			tier: "LC",
+		},
+		illusionmgc: {
+			tier: "OU",
+		},
+		parrotdragon: {
+			tier: "OU",
+		},
+		trihorndragon: {
+			tier: "OU",
+		},
+		mystictomato: {
+			tier: "LC",
+		},
+		hungryburger: {
+			tier: "OU",
+		},
+		thunderdragon: {
+			tier: "LC",
+		},
+		twinthunderd: {
+			tier: "NFE",
+		},
+		tdtitan: {
+			tier: "OU",
+		},
+		tdcolossus: {
+			tier: "OU",
+		},
+		pantherwarrior: {
+			tier: "OU",
+		},
+		musicking: {
+			tier: "LC",
+		},
+		heavymetalking: {
+			tier: "OU",
+		},
+		slotmachine: {
+			tier: "OU",
+		},
+		battleox: {
+			tier: "LC",
+		},
+		rabidhorseman: {
+			tier: "OU",
+		},
+		jiraigumo: {
+			tier: "LC",
+		},
+		launcherspider: {
+			tier: "OU",
+		},
+		vampirebat: {
+			tier: "LC",
+		},
+		vampirelord: {
+			tier: "NFE",
+		},
+		vampgenesis: {
+			tier: "OU",
+		},
+		vampirelady: {
+			tier: "NFE",
+		},
+		vampgrace: {
+			tier: "OU",
+		},
+		clayman: {
+			tier: "LC",
+		},
+		mudballman: {
+			tier: "OU",
+		},
+		skulldice: {
+			tier: "OU",
+		},
+		orgothtr: {
+			tier: "OU",
+		},
+		lajinn: {
+			tier: "OU",
+		},
+		lavagolem: {
+			tier: "OU",
+		},
+		revivaljam: {
+			tier: "LC",
+		},
+		humanoidslime: {
+			tier: "NFE",
+		},
+		humanoiddrake: {
+			tier: "OU",
+		},
+		godslime: {
+			tier: "OU",
+		},
+		pendulumm: {
+			tier: "OU",
+		},
+		speardragon: {
+			tier: "OU",
+		},
+		silentmg4: {
+			tier: "LC",
+		},
+		silentmg8: {
+			tier: "OU",
+		},
+		silentsw3: {
+			tier: "LC",
+		},
+		silentsw7: {
+			tier: "OU",
+		},
+		exodia: {
+			tier: "Uber",
+		},
+	};
