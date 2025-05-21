06df9d0ecf5afd447cf65ad69fdb66d4b5b7f04f
Cody Spratford <koreanpanda345@gmail.com>
2025-05-20 17:12:30 -0700

    testing something out

--- data/text/pokedex.ts
+++ data/text/pokedex.ts
@@ -1,3 +1,6 @@
 export const PokedexText: { [id: IDEntry]: PokedexText } = {
-
+	illusionm: {
+		name: "Illusion M",
+		desc: "If this card would be destroyed during either player's Battle Phase, you can tribute it to Special Summon 1 \"Dark Magician\" from either your Hand or Deck.",
+	},
 };
