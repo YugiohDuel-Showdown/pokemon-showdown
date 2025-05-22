51fb926bfcc5aaa4784f8107c254eaa392666176
Cody Spratford <koreanpanda345@gmail.com>
2025-05-21 19:09:11 -0700

    ODL going to OU. Lowering Behemoth King

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -307,7 +307,7 @@ export const FormatsData: import("../sim/dex-species").SpeciesFormatsDataTable =
 		tier: "LC",
 	},
 	odlkairyushin: {
-		tier: "Uber",
+		tier: "OU",
 	},
 	harpielady: {
 		tier: "LC",
